7ceab2e2220152f7649d90b20c71cf1eef3a2a5d
khairulharu <khairulaswad719@gmail.com>
2024-04-30 17:43:44 +0800

    new LSP

--- LSP/main.go
+++ LSP/main.go
@@ -11,37 +11,61 @@ type Paket interface {
 	HitungHarga()
 }
 
+// Contoh Provider Jne
 type JNE struct {
-	Harga       int64
-	StatusPaket bool
+	Berat              int64
+	StatusPaketDikirim bool
 }
 
+// Implementasi methos Kirim
 func (j *JNE) Kirim() {
-	j.StatusPaket = true
-	fmt.Printf("Paket sedang di : %v", j.StatusPaket)
+	j.StatusPaketDikirim = true
+	if j.StatusPaketDikirim {
+		fmt.Println("Paket Sudah Dikirim")
+	}
+
+	fmt.Println("PaketBelumDikirim")
 }
 
+// Implementasi methos HitungHarga pada Interface Paket
 func (j *JNE) HitungHarga() {
-	fmt.Println(j.Harga * 5)
+	fmt.Println(j.Berat * 5000)
 }
 
+// Contoh Di Provider Sicepat
 type Sicepat struct {
-	Harga       int64
-	StatusPaket bool
+	Berat              int64
+	StatusPaketDikirim bool
 }
 
+// Implementasi method Kirim
 func (s *Sicepat) Kirim() {
-	s.StatusPaket = true
-	fmt.Printf("Paket sedang di : %v", s.StatusPaket)
+	s.StatusPaketDikirim = true
+	if s.StatusPaketDikirim {
+		fmt.Println("Paket Sudah Dikirim")
+	}
+
+	fmt.Println("PaketBelumDikirim")
 }
 
+// Implementasi Method HitungHarga
 func (s *Sicepat) HitungHarga() {
-	fmt.Println(s.Harga * 7)
+	fmt.Println(s.Berat * 7000)
 }
 
+// Disini kegunaan conthoh kegunaan liskov subsitution Principle
+// Sebuah struct hanya perlu mengimplementasi sebuah interface agar bisa di print
 func PrintStatusDanHarga(p Paket) {
 	p.HitungHarga()
 	p.Kirim()
 }
 func main() {
+	//Inisiate Sebuah variable Denga Struct JNE
+	jne := JNE{
+		StatusPaketDikirim: false,
+		Berat:              9,
+	}
+
+	//Disin hanya perlu memasukkan struct yang sudah mengimplementasi interface Paket
+	PrintStatusDanHarga(&jne)
 }
